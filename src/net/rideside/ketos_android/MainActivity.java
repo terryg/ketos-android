6e7948ce83a2e57ead98d726e795705a2c0c0df1
Terry G Lorber 2 <tgl@rideside.net>
2013-07-30 00:37:01 -0400

    ENH a start

---
+++ src/net/rideside/ketos_android/MainActivity.java
@@ -0,0 +1,100 @@
+package net.rideside.ketos_android;
+
+import twitter4j.Twitter;
+import twitter4j.auth.RequestToken;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+ 
+public class MainActivity extends Activity {
+    // Constants
+    /**
+     * Register your here app https://dev.twitter.com/apps/new and get your
+     * consumer key and secret
+     * */
+    static String TWITTER_CONSUMER_KEY = "LsCQaPOwd8k7WkyRFRZF4Q";
+    static String TWITTER_CONSUMER_SECRET = "KJbJu5IQrlwxW7Cwnax3mMzAc4j3n6Wd2dG125srgk";
+ 
+    // Preference Constants
+    static String PREFERENCE_NAME = "twitter_oauth";
+    static final String PREF_KEY_OAUTH_TOKEN = "oauth_token";
+    static final String PREF_KEY_OAUTH_SECRET = "oauth_token_secret";
+    static final String PREF_KEY_TWITTER_LOGIN = "isTwitterLogedIn";
+ 
+    static final String TWITTER_CALLBACK_URL = "oauth://t4jsample";
+ 
+    // Twitter oauth urls
+    static final String URL_TWITTER_AUTH = "auth_url";
+    static final String URL_TWITTER_OAUTH_VERIFIER = "oauth_verifier";
+    static final String URL_TWITTER_OAUTH_TOKEN = "oauth_token";
+ 
+    // Login button
+    Button btnLoginTwitter;
+    // Update status button
+    Button btnUpdateStatus;
+    // Logout button
+    Button btnLogoutTwitter;
+    // EditText for update
+    EditText txtUpdate;
+    // lbl update
+    TextView lblUpdate;
+    TextView lblUserName;
+ 
+    // Progress dialog
+    ProgressDialog pDialog;
+ 
+    // Twitter
+    private static Twitter twitter;
+    private static RequestToken requestToken;
+     
+    // Shared Preferences
+    private static SharedPreferences mSharedPreferences;
+     
+    // Internet Connection detector
+    private ConnectionDetector cd;
+     
+    // Alert Dialog Manager
+    AlertDialogManager alert = new AlertDialogManager();
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+         
+        cd = new ConnectionDetector(getApplicationContext());
+ 
+        // Check if Internet present
+        if (!cd.isConnectingToInternet()) {
+            // Internet Connection is not present
+            alert.showAlertDialog(MainActivity.this, "Internet Connection Error",
+                    "Please connect to working Internet connection", false);
+            // stop executing code by return
+            return;
+        }
+         
+        // Check if twitter keys are set
+        if(TWITTER_CONSUMER_KEY.trim().length() == 0 || TWITTER_CONSUMER_SECRET.trim().length() == 0){
+            // Internet Connection is not present
+            alert.showAlertDialog(MainActivity.this, "Twitter oAuth tokens", "Please set your twitter oauth tokens first!", false);
+            // stop executing code by return
+            return;
+        }
+ 
+        // All UI elements
+        btnLoginTwitter = (Button) findViewById(R.id.btnLoginTwitter);
+        btnUpdateStatus = (Button) findViewById(R.id.btnUpdateStatus);
+        btnLogoutTwitter = (Button) findViewById(R.id.btnLogoutTwitter);
+        txtUpdate = (EditText) findViewById(R.id.txtUpdateStatus);
+        lblUpdate = (TextView) findViewById(R.id.lblUpdate);
+        lblUserName = (TextView) findViewById(R.id.lblUserName);
+ 
+        // Shared Preferences
+        mSharedPreferences = getApplicationContext().getSharedPreferences("MyPref", 0);
+    }
+}
